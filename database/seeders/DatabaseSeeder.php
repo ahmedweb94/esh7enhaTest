f642117ebb65ee113ddbd1ca7bfc5c9eea874449
ahmmed atef <ahmed.web94@gmail.com>
2021-05-11 16:10:14 +0200

    01

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-//        $this->call(AdminSeeder::class);
+       $this->call(AdminSeeder::class);
 //        $this->call(CitiesSeeder::class);
 //        $this->call(SettingSeeder::class);
-        $this->call(PermissionSeeder::class);
+        // $this->call(PermissionSeeder::class);
 
     }
 }
